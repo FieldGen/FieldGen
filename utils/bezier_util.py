37d9b96bae3f96978b9b40a5dfcecd944b6fdc2b
keheye <keheye2002@gmail.com>
2025-08-12 20:26:28 +0800

    fix: add use method
    Signed-off-by: keheye <keheye2002@gmail.com>

--- utils/bezier_util.py
+++ utils/bezier_util.py
@@ -6,7 +6,7 @@ def point_to_plane_distance(point, plane_normal, plane_point):
     return np.abs(np.dot(plane_normal, point - plane_point)) / np.linalg.norm(plane_normal)
 
 def bezier_curve(points, num=100):
-    # 三次贝塞尔曲线
+    # 三次方贝塞尔曲线
     n = len(points) - 1
     t = np.linspace(0, 1, num)
     curve = np.zeros((num, 3))
@@ -30,41 +30,38 @@ def find_nearest_points(curve, sample_points, m):
                 break
         if len(result) == m:
             break
-    # 如果还不够，补足
-    if len(result) < m:
-        for idx in range(len(sample_points)):
-            if idx not in used:
-                result.append(sample_points[idx])
-                if len(result) == m:
-                    break
     return result
 
 def generate_bezier_trajectory(start, end, sample_points, m, safe_plane_x=0):
-    # start, end: np.array([x, y, z])
+    """
+    生成平滑的贝塞尔轨迹，轨迹采样点只从输入采样点中选取。
+    输入:
+        start: np.array([x, y, z])，起点
+        end: np.array([x, y, z])，终点
+        sample_points: [{'img':..., 'state':..., 'xyz':np.array([x,y,z])}, ...]，空间采样点
+        m: 采样点数量
+        safe_plane_x: 安全平面x坐标，默认0
+    输出:
+        traj: 轨迹采样点（只包含输入采样点中的点，长度为m）
+        grasp_point: 抓取点坐标（即end）
+        safe_plane_dist: 抓取点到安全平面的距离
+    """
     # 判断起点在安全平面左还是右
     if start[0] < safe_plane_x:
         # 左侧，5点
-        # P0: 起点垂直于安全平面的点
         P0 = np.array([safe_plane_x, start[1], start[2]])
-        # P1: P0对抓取点水平面的垂直点
         P1 = np.array([safe_plane_x, end[1], start[2]])
-        # P2: P1垂直抓取点垂直安全平面的那条射线l的垂直点
-        # 射线l: (x=safe_plane_x, y=end[1], z)
         P2 = np.array([safe_plane_x, end[1], end[2]])
         control_points = [start, P0, P1, P2, end]
     else:
         # 右侧，4点
-        # P0: 起点对抓取点水平面的垂直点
         P0 = np.array([start[0], end[1], start[2]])
-        # P1: P0垂直抓取点垂直安全平面的那条射线l的垂直点
-        # 射线l: (x, y=end[1], z=end[2])
         P1 = np.array([start[0], end[1], end[2]])
         control_points = [start, P0, P1, end]
     # 生成贝塞尔曲线
     curve = bezier_curve(control_points, num=200)
-    # 采样m个最近点
+    # 采样m个最近点（只用输入采样点）
     traj = find_nearest_points(curve, sample_points, m)
-    # 输出
     grasp_point = end
     safe_plane_dist = point_to_plane_distance(end, np.array([1,0,0]), np.array([safe_plane_x,0,0]))
     return traj, grasp_point, safe_plane_dist
