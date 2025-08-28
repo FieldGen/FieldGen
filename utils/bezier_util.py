3a407431150345001e1103f5fec58d4a5252e18d
keheye <keheye2002@gmail.com>
2025-08-28 11:35:07 +0800

    Update: support vertical generate

--- utils/bezier_util.py
+++ utils/bezier_util.py
@@ -15,20 +15,34 @@ def bezier_curve(points, num):
         curve += binom * ((1 - t) ** (n - i))[:, None] * (t ** i)[:, None] * points[i]
     return curve
 
-def generate_bezier_trajectory(start, end, num=200):
-    safe_plane_y = end[1]
+def generate_bezier_trajectory(start, end, num=200, vertical = True):
     # 判断起点在安全平面左还是右
-    if start[1] > safe_plane_y:
-        # 左侧，5点
-        P0 = np.array([start[0], safe_plane_y, start[2]])
-        P1 = np.array([start[0], safe_plane_y, end[2]])
-        P2 = np.array([end[0], safe_plane_y, end[2]])
-        control_points = [start, P0, P1, P2, end]
+    if vertical==False:
+        safe_plane_y = end[1]
+        if start[1] > safe_plane_y:
+            # 左侧，5点
+            P0 = np.array([start[0], safe_plane_y, start[2]])
+            P1 = np.array([start[0], safe_plane_y, end[2]])
+            P2 = np.array([end[0], safe_plane_y, end[2]])
+            control_points = [start, P0, P1, P2, end]
+        else:
+            # 右侧，4点
+            P0 = np.array([start[0], start[1], end[2]])
+            P1 = np.array([end[0], start[1], end[2]])
+            control_points = [start, P0, P1, end]
     else:
-        # 右侧，4点
-        P0 = np.array([start[0], start[1], end[2]])
-        P1 = np.array([end[0], start[1], end[2]])
-        control_points = [start, P0, P1, end]
+        safe_plane_z = end[2]
+        if start[2] < safe_plane_z:
+            # 左侧，5点
+            P0 = np.array([start[0], start[1], safe_plane_z])
+            P1 = np.array([start[0], end[1], safe_plane_z])
+            P2 = np.array([end[0], end[1], safe_plane_z])
+            control_points = [start, P0, P1, P2, end]
+        else:
+            # 右侧，4点
+            P0 = np.array([start[0], end[1], start[2]])
+            P1 = np.array([end[0], end[1], start[2]])
+            control_points = [start, P0, P1, end]
     # 生成贝塞尔曲线
     curve = bezier_curve(control_points, num)
     # 采样m个最近点（只用输入采样点）
